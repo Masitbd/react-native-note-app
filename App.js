03b9240298bcef1a3e737c4c3297bb655c0e10a6
sarker24 <sarker24@gmail.com>
2023-12-06 21:45:46 +0100

    Add firebase

--- App.js
+++ App.js
@@ -7,6 +7,21 @@ import SignIn from './src/screens/sign-in';
 import SignUp from './src/screens/sign-up';
 import Edit from './src/screens/edit';
 import Create from './src/screens/create';
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBDpTfHmNlrxpuooa2KVyMO9J2H7w1nQ1g",
+  authDomain: "react-native-note-app-4fd92.firebaseapp.com",
+  projectId: "react-native-note-app-4fd92",
+  storageBucket: "react-native-note-app-4fd92.appspot.com",
+  messagingSenderId: "308090357770",
+  appId: "1:308090357770:web:147093c909f06438ddf7f3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 const AppTheme = {
   ...DefaultTheme,
